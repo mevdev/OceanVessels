30fda660d0c84880405ddbd923506fcf49d70664
Robert Linnemann <robertlinnemann@gmail.com>
2016-04-24 21:38:35 -0700

    first commit

---
+++ VesselLookup/SearchWindowController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchWindowController.swift
+//  VesselLookup
+//
+//  Created by Robert Linnemann on 4/23/16.
+//  Copyright © 2016 Epistrophy. All rights reserved.
+//
+
+import Foundation
---
+++ VesselLookup/VesselWindowController.swift
@@ -0,0 +1,9 @@
+//
+//  VesselWindowController.swift
+//  VesselLookup
+//
+//  Created by Robert Linnemann on 4/23/16.
+//  Copyright © 2016 Epistrophy. All rights reserved.
+//
+
+import Foundation
